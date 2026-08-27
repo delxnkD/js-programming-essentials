5f16db39c455cafc7a4c0b608b0a770382c4e9c2
delxnkD <delankdavis@gmail.com>
2026-08-27 09:14:54 -0400

    To do list

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -2,7 +2,7 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
-
+const clearAllTasksBtn = document.getElementById("clearAllTasksBtn");
 let tasks = [];
 
 function addTask() {
@@ -32,8 +32,14 @@ function clearCompletedTasks() {
             tasks = tasks.filter(task => !task.completed);
             displayTasks();
         }
+function clearAllTasks() {
+            tasks = [];
+            displayTasks();
+        }
+
 
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllTasksBtn.addEventListener("click", clearAllTasks);
 
 displayTasks();
